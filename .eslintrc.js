e65ac5a3f60a3bad2378f7e3bf1ed68ea9025005
Fernando Araujo <fernando.araujo.filho@tenderframe.de>
2020-08-06 21:26:36 +0200

    new structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
+    'airbnb-base',
   ],
   root: true,
   env: {
@@ -17,8 +18,42 @@ module.exports = {
     jest: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': ['error', 'never'],
+    'class-methods-use-this': 'off',
+    semi: 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'no-tabs': 'off',
+    'no-use-before-define': 0,
+    'no-console': 1,
+    'no-undef': 0,
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'error',
+    'no-underscore-dangle': 0,
+    'import/prefer-default-export': 0,
+    'import/imports-first': ['error', 'absolute-first'],
+    'import/newline-after-import': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/', 'scripts'],
+      },
+    },
   },
 };
