fc50816abaa48759d60041085f88fa58295398e7
Fernando Araujo <fernando.araujo.filho@tenderframe.de>
2020-08-13 16:49:38 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,12 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { UserEntity } from '../models/users/user.entity'
 import { CardModule } from '../modules/card.module'
 import { UserModule } from '../modules/user.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_URL,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_URL_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [UserEntity],
-      synchronize: false,
-    }),
-    UserModule,
-    CardModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), UserModule, CardModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app/app.module'
+
+const port = process.env.PORT || 8080
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // app.enableCors()
-  await app.listen(3000)
+  await app.listen(port)
+  Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap')
 }
 bootstrap()
